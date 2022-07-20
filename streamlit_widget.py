9d3e007ed3b627123a67310437640b3420b3441a
danOSU <liyanage.5@osu.edu>
2022-07-20 01:17:52 -0400

    final version of emulators for VAH project

--- streamlit_widget.py
+++ streamlit_widget.py
@@ -1,8 +1,9 @@
 # %%
+from immutables import Map
 import streamlit as st
 import numpy as np
 import time
-import os
+import os, sys
 import subprocess
 import matplotlib
 import altair as alt
@@ -18,7 +19,24 @@ from sklearn.gaussian_process import GaussianProcessRegressor as gpr
 from sklearn.gaussian_process import kernels as krnl
 import seaborn as sns
 
-npc=3
+
+sys.path.append('../visualization_vah/surmise/')
+#sys.path.append('/Users/dananjayaliyanage/git/analyze_vah/emulation')
+from surmise import *
+import pickle
+import pyximport
+pyximport.install(setup_args={"include_dirs":np.get_include()},
+                  reload_support=True)
+                  
+cal_name = 'VAH_' + 'PCSK' + '_calibrator_PTMC'
+with open(cal_name + '.pkl' , 'rb') as file:
+    cal = pickle.load(file)   
+
+x_np = np.arange(0, 98)[:, None]
+x_np = x_np.astype('object')
+
+
+#npc=4
 
 # %%
 # https://gist.github.com/beniwohli/765262
@@ -57,7 +75,7 @@ short_names = {
 
 # %%
 system_observables = {
-                    'Pb-Pb-2760' : ['dNch_deta', 'dET_deta', 'dN_dy_pion', 'dN_dy_kaon' ,'dN_dy_proton', 'mean_pT_pion','mean_pT_kaon', 'mean_pT_proton']
+                    'Pb-Pb-2760' : ['dNch_deta', 'dET_deta', 'dN_dy_pion', 'dN_dy_kaon' ,'dN_dy_proton', 'mean_pT_pion','mean_pT_kaon', 'mean_pT_proton', 'v22', 'v32', 'v42']
                     }
 
 # %%
@@ -82,7 +100,7 @@ obs_word_labels = {
 system = 'Pb-Pb-2760'
 
 # %%
-@st.cache(allow_output_mutation=True, show_spinner=False)
+#@st.cache(allow_output_mutation=True, show_spinner=False)
 def load_emu():
     #load the emulator
     with open('PbPb2760_vah_emulators.dat',"rb") as f:
@@ -90,7 +108,27 @@ def load_emu():
     return emu
 
 # %%
-def predict_observables(model_parameters, Emulators, inverse_tf_matrix, SS_mean):
+# def predict_observables(model_parameters, Emulators, inverse_tf_matrix, SS_mean):
+#     mean=[]
+#     variance=[]
+#     theta=np.array(model_parameters).flatten()
+
+#     if len(theta)!=15:
+#         raise TypeError('The input model_parameters array does not have the right dimensions')
+#     else:
+#         theta=np.array(theta).reshape(1,15)
+#         for i in range(0,npc):
+#             mn,std=Emulators[i].predict(theta,return_std=True)
+#             mean.append(mn)
+#             variance.append(std**2)
+#     mean=np.array(mean).reshape(1,-1)
+#     inverse_transformed_mean=mean @ inverse_tf_matrix + np.array(SS_mean).reshape(1,-1)
+#     variance_matrix=np.diag(np.array(variance).flatten())
+#     A_p=inverse_tf_matrix
+#     inverse_transformed_variance=np.einsum('ik,kl,lj-> ij', A_p.T, variance_matrix, A_p, optimize=False)
+#     return inverse_transformed_mean, inverse_transformed_variance
+
+def predict_observables(model_parameters, cal):
     mean=[]
     variance=[]
     theta=np.array(model_parameters).flatten()
@@ -99,28 +137,31 @@ def predict_observables(model_parameters, Emulators, inverse_tf_matrix, SS_mean)
         raise TypeError('The input model_parameters array does not have the right dimensions')
     else:
         theta=np.array(theta).reshape(1,15)
-        for i in range(0,npc):
-            mn,std=Emulators[i].predict(theta,return_std=True)
-            mean.append(mn)
-            variance.append(std**2)
-    mean=np.array(mean).reshape(1,-1)
-    inverse_transformed_mean=mean @ inverse_tf_matrix + np.array(SS_mean).reshape(1,-1)
-    variance_matrix=np.diag(np.array(variance).flatten())
-    A_p=inverse_tf_matrix
-    inverse_transformed_variance=np.einsum('ik,kl,lj-> ij', A_p.T, variance_matrix, A_p, optimize=False)
-    return inverse_transformed_mean, inverse_transformed_variance
+    prediction = cal.emu.predict(x=x_np, theta=theta.flatten())
+    return prediction.mean().flatten(), prediction.covx().reshape(98,98)
 
 
 # %%
+# @st.cache(allow_output_mutation=True, show_spinner=False)
+# def emu_predict(emu, params,inverse_tf_matrix, SS_mean):
+#     start = time.time()
+#     Yemu_cov = 0
+#     #Yemu_mean = emu.predict( np.array( [params] ), return_cov=False )
+#     Yemu_mean, Yemu_cov = predict_observables( np.array( [params] ), emu ,inverse_tf_matrix, SS_mean)
+#     end = time.time()
+#     time_emu = end - start
+#     return Yemu_mean, Yemu_cov, time_emu'
+
 @st.cache(allow_output_mutation=True, show_spinner=False)
-def emu_predict(emu, params,inverse_tf_matrix, SS_mean):
+def emu_predict(cal, params):
     start = time.time()
     Yemu_cov = 0
     #Yemu_mean = emu.predict( np.array( [params] ), return_cov=False )
-    Yemu_mean, Yemu_cov = predict_observables( np.array( [params] ), emu ,inverse_tf_matrix, SS_mean)
+    Yemu_mean, Yemu_cov = predict_observables( np.array( [params] ), cal)
     end = time.time()
     time_emu = end - start
     return Yemu_mean, Yemu_cov, time_emu
+
 
 # %%
 ALICE_cent_bins = np.array([[0,5],[5,10],[10,20],[20,30],[30,40],[40,50],[50,60],[60,70]])
@@ -149,6 +190,44 @@ obs_cent_list = {
     'v42' : np.array([[0,5],[5,10],[10,20],[20,30], [30,40],[40,50]]), # 6 bins
     }
 }
+
+obs_groups = {'yields' : ['dNch_deta', 'dET_deta', 'dN_dy_pion', 'dN_dy_kaon', 'dN_dy_proton'],
+              'mean_pT' : ['mean_pT_pion', 'mean_pT_kaon','mean_pT_proton', ],
+              'fluct' : ['pT_fluct'],
+              'flows' : ['v22', 'v32', 'v42']}
+
+obs_group_labels = {'yields' : r'$dN_\mathrm{id}/dy_p$, $dN_\mathrm{ch}/d\eta$, $dE_T/d\eta$ [GeV]',
+                    'mean_pT' : r'$ \langle p_T \rangle_\mathrm{id}$' + ' [GeV]',
+                    'fluct' : r'$\delta p_{T,\mathrm{ch}} / \langle p_T \rangle_\mathrm{ch}$',
+                    'flows' : r'$v^{(\mathrm{ch})}_k\{2\} $'}
+
+colors = ['b', 'g', 'r', 'c', 'm', 'tan', 'gray']
+
+obs_tex_labels = {'dNch_deta' : r'$dN_\mathrm{ch}/d\eta$',
+                  'dN_dy_pion' : r'$dN_{\pi}/dy_p$',
+                  'dN_dy_kaon' : r'$dN_{K}/dy_p$',
+                  'dN_dy_proton' : r'$dN_{p}/dy_p$',
+                  'dET_deta' : r'$dE_{T}/d\eta$',
+
+                  'mean_pT_proton' : r'$\langle p_T \rangle_p$',
+                  'mean_pT_kaon' : r'$\langle p_T \rangle_K$',
+                  'mean_pT_pion' : r'$\langle p_T \rangle_\pi$',
+
+                  'pT_fluct' : r'$\delta p_{T,\mathrm{ch}} / \langle p_T \rangle_\mathrm{ch}$',
+                  'v22' : r'$v^{(\mathrm{ch})}_2\{2\}$',
+                  'v32' : r'$v^{(\mathrm{ch})}_3\{2\}$',
+                  'v42' : r'$v^{(\mathrm{ch})}_4\{2\}$'}
+
+
+
+index={}
+st_index=0
+for obs_group in  obs_groups.keys():
+    for obs in obs_groups[obs_group]:
+        n_centrality = len(obs_cent_list['Pb-Pb-2760'][obs])
+        index[obs]=[st_index,st_index+n_centrality]
+        st_index = st_index + n_centrality
+#index
 
 
 # %%
@@ -184,20 +263,27 @@ def main():
 
     #load the design
     #design, labels, design_max, design_min = load_design(system)
-    inverse_tf_matrix = np.load('INV_MAT.npy')
-    SS_mean = np.load('SS_MN.npy')
+
+    #######################################################
+    #inverse_tf_matrix = np.load('INV_MAT.npy')
+    #SS_mean = np.load('SS_MN.npy')
     load_prior = pd.read_csv('priorVAH.csv', index_col=0)
     maxar = load_prior.loc['max'].values
     minar = load_prior.loc['min'].values
+
+
     #design_min = [10, -0.7, 0.5, 0, 0.3, 0.135, 0.13, 0.01, -2, -1, 0.01, 0.12, 0.025, -0.8, 0.3]
     #design_max = [30, 0.7, 1.5, 4.91, 2, 0.165, 0.3, 0.2, 1, 2, 0.25, 0.3, 0.15, 0.8, 1]
     #load the emu
-    emu = load_emu()
+
+
+    #emu = load_emu()
 
     #load the exp obs
     experiment=pd.read_csv(filepath_or_buffer="PbPb2760_experiment",index_col=0)
-    y_exp= experiment.values[0,0:-32]
-    y_exp_variance= experiment.values[1,0:-32]
+    experiment = experiment.drop(columns=experiment.keys()[index['pT_fluct'][0]:index['pT_fluct'][1]])
+    y_exp= experiment.values[0,:]
+    y_exp_variance= experiment.values[1,:]
     observables = experiment.keys()
     exp_label=[]
     nobs = len(observables)
@@ -209,16 +295,19 @@ def main():
     #print(design_min)
     #print(design_max)
     #updated params
-    MAP = np.array([16.57112226,  0.05712234,  1.18538221,  0.86145252,  0.98799686,
-        0.14898391,  0.20865709,  0.1121545 , -1.12117563,  1.58534083,
-        0.14337955,  0.17982499,  0.09039792,  0.12835018,  0.6408514 ])
-    MAP_mike = np.array([20.0, 0.063, 1.05, 0.98, 1.44, 0.136, 0.233, 0.096,
+    MAP = np.array([24.13700415, -0.19453601,  0.97201516,  0.92780229,  1.26233326, 0.13507499,  0.28643515,  0.05385407, -0.6567839 ,  0.55309067, 
+    0.11020714,  0.14247949,  0.03355921, -0.73659538,  0.53047712])
+    # MAP with only 50 designs is the following
+    #MAP = np.array([16.57112226,  0.05712234,  1.18538221,  0.86145252,  0.98799686,
+    #    0.14898391,  0.20865709,  0.1121545 , -1.12117563,  1.58534083,
+    #    0.14337955,  0.17982499,  0.09039792,  0.12835018,  0.6408514 ])
+    MAP_mike = np.array([20.0, 0.063, 0.98, 1.44, 1.05 , 0.136, 0.233, 0.096,
                             -0.776, 0.37, 0.133, 0.12, 0.072, -0.122, 0.3])
 
     #MAP = np.array([26.15362668, -0.32770818,  0.803825  ,  0.71214722,  1.35978106,
     #    0.15720997,  0.18496215,  0.04190095,  0.51648601,  0.12397726,
     #    0.05252478,  0.19255562,  0.06805631,  0.08109898,  0.58375491])
-    init_param = st.sidebar.selectbox('Select model parametrs',['Best guess', 'MAP'])
+    #init_param = st.sidebar.selectbox('Select model parametrs',['Best guess', 'MAP'])
     frz = st.sidebar.checkbox('Freeze MAP predictions in green')
 
     for i_s, s_name in enumerate(short_names.keys()):
@@ -227,25 +316,27 @@ def main():
         #ave_val = np.zeros(15)
         #print(ave_val.tolist())
         #print(type(ave_val.tolist()))
-        if init_param == 'Best guess':
-            st.sidebar.text('MAP values are in bracket')
-            p = st.sidebar.slider(s_name+f'  ({MAP[i_s]:.3f})', min_value= float(minar[i_s]), max_value= float(maxar[i_s]),
-                                value=float(MAP_mike[i_s]), step= 0.00001)
-        if init_param == 'MAP':
-            st.sidebar.text('Best guess values are in bracket')
-            p = st.sidebar.slider(s_name+f'  ({MAP_mike[i_s]:.3f})', min_value= float(minar[i_s]), max_value= float(maxar[i_s]),
-                                value=float(MAP[i_s]), step= 0.00001)
+        #if init_param == 'Best guess':
+        #    st.sidebar.text('MAP values are in bracket')
+        #    p = st.sidebar.slider(s_name+f'  ({MAP[i_s]:.3f})', min_value= float(minar[i_s]), max_value= float(maxar[i_s]),
+        #                        value=float(MAP_mike[i_s]), step= 0.00001)
+        #if init_param == 'MAP':
+        #st.sidebar.text('Best guess values are in bracket')
+        #p = st.sidebar.slider(s_name+f'  ({MAP_mike[i_s]:.3f})', min_value= float(minar[i_s]), max_value= float(maxar[i_s]),
+        p = st.sidebar.slider(s_name, min_value= float(minar[i_s]), max_value= float(maxar[i_s]),
+
+        value=float(MAP[i_s]), step= 0.00001)
         params.append(p)
 
     #get emu prediction
-    Yemu_mean, Yemu_cov, time_emu = emu_predict(emu, params, inverse_tf_matrix, SS_mean)
+    Yemu_mean, Yemu_cov, time_emu = emu_predict(cal,params)
     if frz == 1:
-            Yemu_mean_MAP, Yemu_cov_MAP, time_emu_MAP = emu_predict(emu, MAP, inverse_tf_matrix, SS_mean)
+            Yemu_mean_MAP, Yemu_cov_MAP, time_emu_MAP = emu_predict(cal, MAP)
     nplots = len(system_observables['Pb-Pb-2760'])
     #print(nplots)
     sns.set_context('poster')
     sns.set_style('whitegrid')
-    fig, axs = plt.subplots(2,4,figsize=(25,15))
+    fig, axs = plt.subplots(3,4,figsize=(25,25))
     axs = axs.flatten()
     last_obs = 0
     for i in range(0,nplots):
@@ -272,7 +363,7 @@ def main():
             #cen_bin_mid = np.arange(0,len(cen_bins))
             ax.errorbar(cen_bin_mid, y_values_MAP , y_pred_err_MAP, color = 'green')
 
-        ax.set_title(system_observables['Pb-Pb-2760'][i])
+        ax.set_title(obs_tex_labels[system_observables['Pb-Pb-2760'][i]])
         if i==1:
             cen_bin_mid = cen_bin_mid[0:-1:2]
             cen_bins = cen_bins[0:-1:2]
@@ -290,13 +381,13 @@ def main():
         st.markdown('The observables above (and additional ones not shown) are combined into [principal components](https://en.wikipedia.org/wiki/Principal_component_analysis) (PC).')
         st.markdown('A [Gaussian Process](https://en.wikipedia.org/wiki/Gaussian_process) (GP) is fitted to each of the dominant principal components by running our physics model on a coarse [space-filling](https://en.wikipedia.org/wiki/Latin_hypercube_sampling) set of points in parameter space.')
         st.markdown('The Gaussian Process is then able to interpolate between these points, while estimating its own uncertainty.')
-    st.markdown("How does simulation error compare with the experimental error?")
-    chk = st.selectbox('Check it for',['20 observables with biggest error', 'All observables'])
-    if st.button('Play video'):
-        if chk == '20 observables with biggest error':
-            st.image('animation.gif')
-        else:
-            st.image('animation_all.gif')
+    # st.markdown("How does simulation error compare with the experimental error?")
+    # chk = st.selectbox('Check it for',['20 observables with biggest error', 'All observables'])
+    # if st.button('Play video'):
+    #     if chk == '20 observables with biggest error':
+    #         st.image('animation.gif')
+    #     else:
+    #         st.image('animation_all.gif')
     #st.markdown('To update the widget with latest changes, click the button below, and then refresh your webpage.')
     #if st.button('(Update widget)'):
     #    subprocess.run("git pull origin master", shell=True)s
